bf5b6e1c80df1feaf48a8b7ed39ad4f3de4760d0
moha0950 <moha0950@umn.edu>
2023-06-17 19:30:26 -0700

    initial commit - ready for a great project idea :)

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Login from './login.js';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Login);
---
+++ src/screens/login.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Perform your login logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Add your login logic to authenticate the user
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+export default Login;
